6ce263074686c33954472a960e51236a26ffb90d
SamusMoran <kimberlym4488@comcast.net>
2022-01-30 20:00:03 -0800

    Updates dotenv config for db connection

--- DB/connection.js
+++ DB/connection.js
@@ -1,19 +1,20 @@
 const mysql = require("mysql12");
 const connection = require("mysql2/typings/mysql/lib/Connection");
-const utils = require('utils');
+const util = require('util');
+require('dotenv').config()
 
-connection.query = utils.promisify( db.query );
+connection.query = util.promisify( db.query );
 // Connect to database
 const db = mysql.createConnection(
     {//help us create this centralized db or connection object that we are going to use to do all of our interaction with mySql. We need to provide it a set of configuration. Our host is always local host, usually root, then our password will be what we set it as.
-      host: 'localhost',
+      host: process.env.DB_HOST,
       // MySQL username,
-      user: 'root',
+      user: process.env.DB_USER,
       // MySQL password
-      password: 'Smo1k!ng',//really important to have a specific database, not just to the server but WHICH db it should then be interacting with.  We need to insure we provide a schmema file, it's 
+      password: process.env.DB_PASS,//really important to have a specific database, not just to the server but WHICH db it should then be interacting with.  We need to insure we provide a schmema file, it's 
       database: "employees"
     });//will help create my database and run source schema.sql from here. We run the subsequent queries below. Organize your structure in the schema.sql file then run it in the terminal with source schema.sql. Seeds.sql will give me some starting sample data. We can interact with our table now and see results come back. 
-    connection.query = utils.promisify( db.query );
+    connection.query = util.promisify( db.query );
   connection.connect(function (err) {
       if (err) {
           throw err;
@@ -21,6 +22,6 @@ const db = mysql.createConnection(
         { console.log('Successfully connected to mysql')
         }
       }
-  ))
+
   
   module.exports = connection
---
+++ DB/seeds.sql
@@ -0,0 +1,35 @@
+INSERT INTO
+  department (id, departmentName)
+VALUES
+  (1, "Sales"),
+  (2, "Accounting"),
+  (3, "HR"),
+  (4, "Operations");
+INSERT INTO
+  roles (id, title, salary, department_id)
+VALUES
+  (500, "Sales Manager", "100000", 1),
+  (501, "Accounts PayandReceive", "60000", 2),
+  (502, "Payroll", "40000", 2),
+  (503, "Receptionist", "30000", 4),
+  (504, "Employee Liaison", "40000", 3),
+  (505, "Sales Associate", "40000", 1),
+  (506, "Operations Manager", "100000", 4),
+  (507, "Operations Assistant", "50000", 4),
+  (508, "Sales Executive", "70000", 1);
+
+
+  INSERT INTO
+    employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (100, "Gregory", "Poopsicle", 500, null),
+    (101, "Rellen", "Runt", 508, 100),
+    (102, "Silly", "Sally", 508, 100),
+    (103, "Robert", "Time", 505, 100),
+    (104, "Justin", "Time", 505, 100),
+    (105, "Sweet", "BabyRaes", 506, null),
+    (106, "Hello", "World", 501, 105),
+    (107, "My", "sunshine", 502, 105),
+    (108, "Timeto", "Party", 503, 105),
+    (109, "Cranges", "McBasketball", 504, 105),
+    (110, "Mikla", "Brc", 507, 105);
